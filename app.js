701b96506a1eae56351c5789d4cbcd2491edefa1
Sashank Tungaturthi <devil.sas.007@gmail.com>
2019-11-19 15:52:55 -0700

    Add points interface

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const request = require("request");
 const querystring = require("querystring");
 const MongoClient = require("mongodb").MongoClient;
 var Pusher = require("pusher");
+var socket = require("socket.io");
+
+// var server_ = require("http").createServer(app);
+var port = process.env.PORT || 5000;
 
 const AuthConfig = require("./config");
 const redirect_uri = `${AuthConfig.HOST}/callback`;
@@ -34,6 +38,7 @@ const client = new MongoClient(mongoConnStr, {
 });
 let db;
 
+//pusher credentials
 var pusher = new Pusher({
   appId: "882030",
   key: "a3ef4965765d2b7fea88",
@@ -47,42 +52,56 @@ const createConn = async () => {
   db = client.db("Spotiq");
 };
 
-app.post("/router", (req, res) => {
-  const asyncTrigger = msg => {
-    return new Promise((resolve, reject) => {
-      pusher.trigger("queue-channel", "queue-item", msg, (err, req, res) => {
-        if (err) {
-          reject(err);
-        }
-        resolve(res);
-      });
+const pushNotification = async msg => {
+  return await asyncTrigger(msg);
+};
+
+const asyncTrigger = msg => {
+  console.log("asyn trigger...");
+  return new Promise((resolve, reject) => {
+    pusher.trigger("queue-channel", "queue-item", msg, (err, req, res) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(res);
     });
-  };
+  });
+};
 
-  const pushNotification = async msg => {
-    return await asyncTrigger(msg);
-  };
+app.post("/queue", (req, res) => {
+  // pushNotification({ queue: req.body.queue, privateId: req.body.privateId });
 
-  pushNotification({ queue: req.body.queue, privateId: req.body.privateId });
-  // channels_client.trigger("queue-channel", "queue-item", {
-  //   queue: req.body.queue,
-  //   privateId: req.body.privateId
-  // });
   return res.json({ success: true, message: "Added item to queue" });
 });
 
-const performQueryUpdateUsers = async (privateId, userName, userId) => {
+const performQueryUpdateUsers = async (privateId, userName, userId, points) => {
   const users = db.collection("users");
 
   const newUser = {
     privateId,
     userName,
-    userId
+    userId,
+    points
   };
 
   return {
     insertedUser: newUser,
     mongoResult: await users.insertOne(newUser)
+  };
+};
+
+const performQueryUpdateQueues = async (privateId, queue, master) => {
+  const queues = db.collection("queues");
+
+  const newQueue = {
+    privateId,
+    queue,
+    master
+  };
+
+  return {
+    insertedQueue: newQueue,
+    mongoResult: await queues.insertOne(newQueue)
   };
 };
 
@@ -99,7 +118,7 @@ var generateRandomString = function(length) {
 
 //Handle the GET endpoint on the root route /
 app.get("/", async (req, res, next) => {
-  res.status(200).send("Hello Serverless!");
+  res.status(200).send("Welcome to SpotiQ!");
 });
 
 //Handle the GET endpoint on the root route /
@@ -112,6 +131,7 @@ app.post("/new_user", async (req, res, next) => {
   let privateId = req.body.privateId;
   let userName = req.body.userName;
   let userId = req.body.userId;
+  let points = req.body.points;
 
   if (!client.isConnected()) {
     // Cold start or connection timed out. Create new connection.
@@ -125,40 +145,9 @@ app.post("/new_user", async (req, res, next) => {
     }
   }
   try {
-    res.json(await performQueryUpdateUsers(privateId, userName, userId));
-    return;
-  } catch (e) {
-    res.send({
-      error: e.message
-    });
-    return;
-  }
-});
-
-app.post("/get_user_id", async function(req, res) {
-  var id = req.body.privateId;
-  if (!client.isConnected()) {
-    // Cold start or connection timed out. Create new connection.
-    try {
-      await createConn();
-    } catch (e) {
-      res.json({
-        error: e.message
-      });
-      return;
-    }
-  }
-
-  // Connection ready. Perform insert and return result.
-  try {
-    const users = db.collection("users");
-    const query = { privateId: id };
-    users.find(query).toArray((err, result) => {
-      res.send({
-        search_id: id,
-        users: result
-      });
-    });
+    res.json(
+      await performQueryUpdateUsers(privateId, userName, userId, points)
+    );
     return;
   } catch (e) {
     res.send({
@@ -212,6 +201,167 @@ app.get("/login", function(req, res) {
   );
 });
 
-// app.use("/queue", Queue);
+app.post("/update_user", async function(req, res) {
+  var privateId = req.body.privateId;
+  var userId = req.body.userId;
+  var new_points = req.body.points;
+  if (!client.isConnected()) {
+    // Cold start or connection timed out. Create new connection.
+    try {
+      await createConn();
+    } catch (e) {
+      res.json({
+        error: e.message
+      });
+      return;
+    }
+  }
+
+  // Connection ready. Perform insert and return result.
+  try {
+    const users = db.collection("users");
+    const query = { privateId: privateId, userId: userId };
+
+    //set new value
+    var newvalues = { $set: { points: parseInt(new_points) } };
+    console.log("query: ", query);
+    //update one
+    users.updateOne(query, newvalues, function(err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+    });
+    res.send({ status: 200, message: "Succesfully updated" });
+  } catch (e) {
+    res.send({
+      error: e.message
+    });
+    return;
+  }
+});
+
+app.post("/get_user_by_id", async function(req, res) {
+  let privateId = req.body.privateId;
+  var userId = req.body.playerId;
+  console.log("body for get user: ", req.body);
+  if (!client.isConnected()) {
+    // Cold start or connection timed out. Create new connection.
+    try {
+      await createConn();
+    } catch (e) {
+      res.json({
+        error: e.message
+      });
+      return;
+    }
+  }
+
+  // Connection ready. Perform insert and return result.
+  try {
+    const users = db.collection("users");
+    const query = { userId: userId, privateId: privateId };
+    console.log("query for points: ", query);
+    users.find(query).toArray((err, result) => {
+      console.log("points data for indiv user : ", result);
+      res.send({
+        search_id: privateId,
+        users: result
+      });
+    });
+    return;
+  } catch (e) {
+    res.send({
+      error: e.message
+    });
+    return;
+  }
+});
+
+app.post("/update_queue", async function(req, res) {
+  var id = req.body.privateId;
+  var queue = req.body.queue;
+  var master = req.body.master;
+
+  if (!client.isConnected()) {
+    // Cold start or connection timed out. Create new connection.
+    try {
+      await createConn();
+    } catch (e) {
+      res.json({
+        error: e.message
+      });
+      return;
+    }
+  }
+
+  // Connection ready. Perform insert and return result.
+  try {
+    const queues = db.collection("queues");
+    const query = { privateId: id };
+    var newvalues = {};
+    if (master !== "") newvalues = { $set: { master: master } };
+    //set new value
+    else newvalues = { $set: { queue: queue } };
+
+    //check if there is existing queue
+    queues.find(query).toArray(async (err, result) => {
+      if (result.length > 0) {
+        //update the queue
+        queues.updateOne(query, newvalues, function(err, res) {
+          if (err) throw err;
+        });
+      } else {
+        //create new queue
+        res.json(await performQueryUpdateQueues(id, queue, master));
+      }
+      res.send({
+        search_id: id,
+        queues: result
+      });
+    });
+
+    //update one
+
+    return;
+  } catch (e) {
+    res.send({
+      error: e.message
+    });
+    return;
+  }
+});
+
+app.post("/get_queue", async function(req, res) {
+  var id = req.body.privateId;
+  if (!client.isConnected()) {
+    // Cold start or connection timed out. Create new connection.
+    try {
+      await createConn();
+    } catch (e) {
+      res.json({
+        error: e.message
+      });
+      return;
+    }
+  }
+
+  // Connection ready. Perform insert and return result.
+  try {
+    const queues = db.collection("queues");
+    const query = { privateId: id };
+    queues.find(query).toArray((err, result) => {
+      console.log("result for queues: ", result);
+      res.send({
+        search_id: id,
+        queues: result
+      });
+    });
+    return;
+  } catch (e) {
+    res.send({
+      error: e.message
+    });
+    return;
+  }
+});
 
 module.exports.server = sls(app);
